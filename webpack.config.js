b66742e3bf269d9632305551bee5fbb0823d9d48
braincee <kwesi938@gmail.com>
2022-05-10 21:18:26 +0100

    Fix linter errors

--- webpack.config.js
+++ webpack.config.js
@@ -2,34 +2,34 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: path.resolve(__dirname, 'src/index.js'),
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js',
-        clean: true,  
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    clean: true,
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
     },
-    devServer: {
-       static: {
-           directory: path.resolve(__dirname, 'dist')
-       },
-       open: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                ],
-            },
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          'css-loader',
         ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-           title: 'Leaderboard',
-           template: 'src/index.html',
-        }),
+      },
     ],
-}
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Leaderboard',
+      template: 'src/index.html',
+    }),
+  ],
+};
