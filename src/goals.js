00dc7b4c204210f18a046e1197a0d39e8195f39a
braincee <kwesi938@gmail.com>
2022-05-10 20:10:18 +0100

    Add css styles

--- src/goals.js
+++ src/goals.js
@@ -1,39 +1,39 @@
 const goals = [
-    {
-        name: 'Ronaldo',
-        goals: 40,
-    },
-    {
-        name: 'Salah',
-        goals: 35,
-    },
-    {
-        name: 'Messi',
-        goals: 38,
-    },
-    {
-        name: 'Mbappe',
-        goals: 30,
-    },
-    {
-        name: 'Neymar',
-        goals: 27,
-    },
-    {
-        name: 'Lukaku',
-        goals: 25,
-    },
+  {
+    name: 'Ronaldo',
+    goals: 40,
+  },
+  {
+    name: 'Salah',
+    goals: 35,
+  },
+  {
+    name: 'Messi',
+    goals: 38,
+  },
+  {
+    name: 'Mbappe',
+    goals: 30,
+  },
+  {
+    name: 'Neymar',
+    goals: 27,
+  },
+  {
+    name: 'Lukaku',
+    goals: 25,
+  },
 ];
 
 const players = document.querySelector('.left-section .bottom');
 
 const playerGoals = () => {
-    goals.forEach((goal) => {
-        const player = document.createElement('li');
-        player.className = 'list-item';
-        player.textContent = `${goal.name}: ${goal.goals}`;
-        players.appendChild(player);
-    });
+  goals.forEach((goal) => {
+    const player = document.createElement('li');
+    player.className = 'list-item';
+    player.textContent = `${goal.name}: ${goal.goals}`;
+    players.appendChild(player);
+  });
 };
 
 export default playerGoals;
